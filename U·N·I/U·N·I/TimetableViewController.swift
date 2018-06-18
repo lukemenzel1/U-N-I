308fb2e0ca8c89e9095de21bb9c1e29beb897bfc
Luke Menzel <luke.menzel@student.tsc.nsw.edu.au>
2018-06-18 11:35:54 +1000

    Need to go back to old commit

--- U·N·I/U·N·I/TimetableViewController.swift
+++ U·N·I/U·N·I/TimetableViewController.swift
@@ -8,28 +8,27 @@
 
 import UIKit
 
-class TimetableViewController: UIViewController {
+class TimetableViewController: UIViewController, UIScrollViewDelegate {
 
+    
+    @IBOutlet weak var PageView: UIPageControl!
+    @IBOutlet weak var HorizontalScroll: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        for image in 0...2 {
+            let imageToDisplay = UIImage(named: "\(image).png")
+            let imageView = UIImageView(image: imageToDisplay)
+            
+            
+            let xCoordinate = view.frame.midX + view.frame.width * CGFloat(image)
+            
+            HorizontalScroll.addSubview(imageView)
+            
+            imageView.frame = CGRect(x: xCoordinate, y: view.frame.height / 2, width: 100, height: 100)
+        }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
-
 }
